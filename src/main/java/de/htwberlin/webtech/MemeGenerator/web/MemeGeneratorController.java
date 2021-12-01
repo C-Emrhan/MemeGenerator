75a08a70cef30c0ef67659d2ec0c1115977d95c0
C-Emrhan <77165697+C-Emrhan@users.noreply.github.com>
2021-12-01 18:57:17 +0100

    edit Controller

--- src/main/java/de/htwberlin/webtech/MemeGenerator/web/MemeGeneratorController.java
+++ src/main/java/de/htwberlin/webtech/MemeGenerator/web/MemeGeneratorController.java
@@ -2,25 +2,32 @@ package de.htwberlin.webtech.MemeGenerator.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.HashMap;
 import java.util.List;
 
-
 @RestController
-@RequestMapping(path = "get_memes")
-public class MemeGeneratorController {
+@RequestMapping("/api/v1/")
+public class MemeRestController {
 
     private final MemeService memeService;
-    @Autowired
-    public MemeGeneratorController(MemeService memeService) {
+
+    public MemeRestController(MemeService memeService) {
         this.memeService = memeService;
     }
 
-    @GetMapping
-    public List<Memes> getMemes(){
-        return memeService.getMemes();
+    @GetMapping("memetemplates")
+    public List<Memes> fetchMemeTemplates() {
+        return memeService.fetchTemplates();
     }
+
+    @GetMapping("caption")
+    public HashMap<String, String> createCaptions(@RequestBody MemeCaptionRequest request) {
+        return memeService.captionMeme(request);
+    }
+
 }
