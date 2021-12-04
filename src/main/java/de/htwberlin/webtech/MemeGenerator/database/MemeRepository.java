f180e5aeffab98246898c66f66cf62d2d78ab760
C-Emrhan <77165697+C-Emrhan@users.noreply.github.com>
2021-12-04 13:01:46 +0100

    creating DB table for CreatedMeme

--- src/main/java/de/htwberlin/webtech/MemeGenerator/database/MemeRepository.java
+++ src/main/java/de/htwberlin/webtech/MemeGenerator/database/MemeRepository.java
@@ -1,11 +1,11 @@
 package de.htwberlin.webtech.MemeGenerator.database;
 
-import de.htwberlin.webtech.MemeGenerator.web.Memes;
+import de.htwberlin.webtech.MemeGenerator.web.CreatedMeme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemeRepository extends JpaRepository<Memes,Long> {
+public interface MemeRepository extends JpaRepository<CreatedMeme,String> {
 
 
 }
---
+++ src/main/java/de/htwberlin/webtech/MemeGenerator/web/CreatedMeme.java
@@ -0,0 +1,22 @@
+package de.htwberlin.webtech.MemeGenerator.web;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class CreatedMeme {
+    @Id
+    private String id;
+    private String url;
+
+    public CreatedMeme(String id, String url){
+        this.id = id;
+        this.url = url;
+    }
+
+    public CreatedMeme() {
+
+    }
+}
