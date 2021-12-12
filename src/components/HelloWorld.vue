b39590f9860d1abbd75a00c97b3a1984d999f04b
Canerelpacino <canersmedia@gmail.com>
2021-12-12 19:46:08 +0100

    frontend test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <body>
-    <div class="input-group flex-nowrap">
-      <span class="input-group-text" id="addon-wrapping">@</span>
-      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
-    </div>
-    </body>
   </div>
 </template>
 
 <script>
 export default {
+
   name: 'HelloWorld',
   props: {
     msg: String
@@ -20,5 +15,5 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 </style>
