087eed8155a5b4c49eeaeafde4b05ddec8afc43b
Canerelpacino <canersmedia@gmail.com>
2021-12-19 13:15:33 +0100

    adding submit buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,12 @@
        left: 535px; bottom: 300px"></textarea></div>
     <div class="mm-text-wrap"><textarea placeholder="Text #2" id="tx1" class="mm-text" style="height: 44px; position: absolute;
       left: 535px; bottom: 245px"></textarea></div>
+    <div>&nbsp;</div>
+    <div>
+      <button style="position: absolute; left: 535px; bottom: 190px" type="button" class="btn btn-primary" id="btn" v-on:click="test">Generate</button>
+      &nbsp;
+      <button style="position: absolute; left: 640px; bottom: 190px" type="button" class="btn btn-secondary" v-on:click="resetpage">Reset</button>
+    </div>
     </body>
   </div>
 </template>
