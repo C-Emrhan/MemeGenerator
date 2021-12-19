99ba6519c558f8869b0aa4ed0db0e4af38211747
Canerelpacino <canersmedia@gmail.com>
2021-12-19 22:07:38 +0100

    adding Memetemplates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
     <div class="mm-text-wrap"><textarea placeholder="Text #2" id="tx1" class="mm-text" style="height: 44px; position: absolute;
       left: 535px; bottom: 245px"></textarea></div>
     <div>&nbsp;</div>
+    <div>&nbsp;</div>
+    <div id="slide" style="position: absolute; left: 250px; bottom: 35px">
+      <div v-for="meme in memes" :key="meme.id">
+        <input type="image" :src="meme.url" v-on:click="seephoto(meme)" width="170" height="170">
+      </div>
+    </div>
     <div>
       <button style="position: absolute; left: 535px; bottom: 190px" type="button" class="btn btn-primary" id="btn" v-on:click="test">Generate</button>
       &nbsp;
